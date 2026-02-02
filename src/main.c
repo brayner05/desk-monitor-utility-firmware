e375827072aa5980f3c27608a776c4ed97faa4f3
brayden <braydenrayner@outlook.com>
2026-02-02 19:39:47 -0400

    Added support for the TMP36 temperature sensor. The MCU now reads the temperature from the sensor, formats it, and then displays it in the top right of the display.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define PIN_LCD_D7      PB5
 #define PIN_LCD_D6      PB4
@@ -9,10 +10,10 @@
 #define PIN_LCD_D4      PB2
 #define PIN_LCD_EN      PB1
 #define PIN_LCD_RS      PB0
-#define PIN_TMP32_IN    PC0
+#define PIN_TMP36_IN    PC0
 
 static int impl_put_char(char ch, FILE *stream __attribute__((unused)));
-FILE display_stream_out = FDEV_SETUP_STREAM(impl_put_char, NULL, _FDEV_SETUP_WRITE);
+static FILE display_stream_out = FDEV_SETUP_STREAM(impl_put_char, NULL, _FDEV_SETUP_WRITE); // NOLINT
 
 #define MODE_DATA ((uint8_t) 1)
 #define MODE_CMD  ((uint8_t) 0)
@@ -121,16 +122,60 @@ static void display_init(void) {
 }
 
 static int impl_put_char(const char ch, FILE *stream __attribute__((unused))) {
-    if (ch == '\n') {
+    if (ch == '\n')
         display_goto(0, 1);
-    }
-    else if (ch == '\r') {
+    else if (ch == '\r')
         display_execute(CMD_CURSOR_RETURN);
-    }
-    else {
+    else
         display_send_byte(ch, MODE_DATA);
-    }
     return 0;
+}
+
+/**
+ * @see Section 23.9 (.1) (Register Description) of ATMega328P datasheet.
+ */
+static void adc_init(void) {
+    ADMUX |= _BV(REFS0);    // Set reference voltage to AVCC (5V).
+
+    /*
+     * Set the prescaler value to 128.
+     * The ATMega328P clock runs at 16MHz and AVR ADC needs between 50-200kHz
+     * for accurate readings. 16MHz / 128 = 125kHz which is in that range.
+     */
+    ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);  // Set prescaler value to 128.
+}
+
+static uint16_t adc_read(const uint8_t channel) {
+    // Select channel 0-7.
+    ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
+
+    // Start the conversion:
+    ADCSRA |= _BV(ADSC);
+
+    // Wait for the conversion to finish:
+    while (ADCSRA & _BV(ADSC));
+
+    return ADC;
+}
+
+static void timer1_init(void) {
+    TCCR1A = 0;
+    TCCR1B = _BV(WGM12)             // Clear-Timer-on-Compare (CTC) Mode.
+        | _BV(CS12);                // Set prescaler to 256.
+
+    /*
+     * F_CPU (CPU frequency) is 16MHz.
+     * Prescaler value is 256.
+     * F_CPU / Prescaler = 16MHz / 256 = 62.5kHz.
+     * Tick length = 1 / (F_CPU / Prescaler) = 1 / 62.5kHz = 16us.
+     * Thus, the number of ticks to wait is equal to (milliseconds to wait) / 16us.
+     * For a 500ms interval, ticks = 500ms / 16us = 31250 ticks (0-31249).
+     */
+    const uint16_t max_tick_value = 31249;
+    OCR1A = max_tick_value;
+
+    TIMSK1 |= _BV(OCIE1A);          // Enable Timer1 Compare Interrupt.
+    sei();
 }
 
 static void gpio_init(void) {
@@ -141,12 +186,41 @@ static void gpio_init(void) {
         | _BV(PIN_LCD_D6)
         | _BV(PIN_LCD_D7);
 
-    DDRC &= ~_BV(PIN_TMP32_IN);
+    DDRC &= ~_BV(PIN_TMP36_IN);
+}
+
+static volatile bool adc_ready = false;
+ISR(TIMER1_COMPA_vect) {
+    adc_ready = true;
+}
+
+static void pretty_print_temperature(const float temp_celsius) {
+    const int lcd_width = 16;
+
+    int digits = 1;
+    if (temp_celsius >= 100.0)
+        digits = 3;
+    else if (temp_celsius >= 10.0)
+        digits = 2;
+
+    // Up to 3 digits + the degrees symbol + C:
+    const int start_column = lcd_width - (digits + 2);
+    display_goto(start_column, 0);
+    printf("%d%cC", (int) temp_celsius, (char) 223);
 }
 
 int main(void) {
     gpio_init();
+    timer1_init();
+    adc_init();
     display_init();
-    printf("Hello World!\rNONONO");
-    while (1);
+    while (1) {
+        if (adc_ready) {
+            const uint16_t tmp36_reading = adc_read(PIN_TMP36_IN);
+            const float tmp36_pin_voltage = tmp36_reading * 5.0 / 1023.0;
+            const float temperature_celsius = (tmp36_pin_voltage - 0.5) * 100.0;
+            pretty_print_temperature(temperature_celsius);
+            adc_ready = false;
+        }
+    }
 }
